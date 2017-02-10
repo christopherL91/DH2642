360958e04eb446ff1ccbceb9dae426b91d46fa02
Raghu Nayyar <hey@raghunayyar.com>
2017-02-10 13:27:50 +0100

    A lot of bootstrap in this.

--- src/controllers/description.js
+++ src/controllers/description.js
@@ -2,7 +2,8 @@
 
 export default (view, dinnerModel) => {
     view.addDishButton.addEventListener('click', (e) => {
-        const {id} = e.srcElement.parentElement.dataset;
+        const {id} = e.srcElement.dataset;
+        console.log(id);
         dinnerModel.addDishToMenu(id);
         history.back();
     });
